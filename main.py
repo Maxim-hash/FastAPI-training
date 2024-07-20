2923b3ba70849b8453db02637e267b158d9cd25e
Maxim <eremenko98@yadnex.ru>
2024-07-20 08:19:04 +0700

    Add Pydantic Validation

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+from copy import deepcopy
+from datetime import datetime
+from typing import List, Optional
 from fastapi import FastAPI
 import json
+
+from pydantic import BaseModel, Field
 
 per_page = 20
 
 app = FastAPI(
     title="Training FastAPI"
 )
+
+class Vacancy(BaseModel):
+    id: str
+    requirement: str
+    responsobility: Optional[str]
+    prof_roles: str
+    empoyment: str
+    publishied_at: datetime
+    name: str
+    area_id: int = Field(ge=1)
+    schedule: str
+    exp: str
+    employers_name: str
+    salary: Optional[int]
+    key_skills: Optional[List[str]] = []
 
 with open ("fastAPI.json", "r", encoding='utf-8') as json_file:
         json_data = json.load(json_file)
@@ -19,16 +39,23 @@ def show_stats():
         "amount_vacancsies" : len(jsonify_data)
     }
 
-@app.get("/vacancy")
+@app.get("/vacancies", response_model=List[Vacancy])
 def get_vacansies(limit: int = per_page, page: int = 0):
     return jsonify_data[page*limit:][:limit]
 
-@app.get("/vacancy/{vacancy_id}")
+@app.get("/vacancies/{vacancy_id}", response_model=List[Vacancy])
 def get_vacancy(vacancy_id: str):
     return [vacancy for vacancy in jsonify_data if vacancy.get("id") == vacancy_id]
 
-@app.post("/vacancy/{vacancy_id}")
+@app.post("/vacancies/{vacancy_id}")
 def change_vacancy_name(vacancy_id : str, new_vacancy_name : str):
     current_vacancy = list(filter(lambda vacancy: vacancy.get("id") == vacancy_id, jsonify_data))[0]
     current_vacancy["name"] = new_vacancy_name
     return {"status" : 200, "data" : current_vacancy}
+
+
+
+@app.post("/vacancies")
+def add_vacancies(vacancies: List[Vacancy]):
+    jsonify_data.extend(vacancies)
+    return {"status" : 200, "data" : jsonify_data}
